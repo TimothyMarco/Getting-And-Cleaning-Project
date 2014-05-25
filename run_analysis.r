1b79cf0f1a57a5421d33f9d0ef2c613af7fe25b0
Tim Marco <tim@timmarco.com>
2014-05-25 17:13:33 -0500

    Updated comments on run_analysis.r to make it more readable.

--- run_analysis.r
+++ run_analysis.r
@@ -1,16 +1,22 @@
+# This script assumes that you have downloaded and unzipped all data from this repo.
+
+# Read the variable labels from the original data set.
 labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 # Make the variable names more easily-readable
 labels$V2 <- tolower(labels$V2)
 labels$V2 <- gsub("_","",labels$V2)
 
-
+# Read the activities from features.txt
 features <- read.table("UCI HAR Dataset/features.txt")
 
+# Get the data from the training set
 train.x.values <- read.table("UCI HAR Dataset/train/X_train.txt")
 train.y.values <- read.table("UCI HAR Dataset/train/y_train.txt")
 test.subjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
+
+# Get the data from the test set
 test.x.values <- read.table("UCI HAR Dataset/test/X_test.txt")
 test.y.values <- read.table("UCI HAR Dataset/test/y_test.txt")
 train.subjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
@@ -27,7 +33,7 @@ names(test.x.values) <- features$V2
 levels(test.y.values) <- labels$V2
 levels(train.y.values) <- labels$V2
 
-
+# Combine subjects, activities, and sensor readings.
 test.values <- data.frame(test.subjects,test.y.values,test.x.values)
 train.values <- data.frame(train.subjects,train.y.values,train.x.values)
 
@@ -38,9 +44,11 @@ names(train.values)[1] <- "subject"
 names(test.values)[2] <- "activity"
 names(train.values)[2] <- "activity"
 
+# Merge the two data sets
 merged.data <- rbind(test.values,train.values)
 
 
+# Extract important features (defined as standard deviation and mean measurements)
 important.features <- grep('mean[^F]|std',names(merged.data))
 extracted.features <- merged.data[,important.features]
 extracted.features <- data.frame(merged.data$subject,merged.data$activity,extracted.features)
@@ -51,7 +59,9 @@ names(extracted.features) <- gsub("[[:punct:]]", "", names(extracted.features))
 names(extracted.features)[1] <- "subject"
 names(extracted.features)[2] <- "activity"
 
+# Aggregate averages by subjects and activity to output later
 averages <- aggregate(. ~ subject+activity,data=extracted.features,mean)
 
+# Store the data sets
 tidy.data <- write.csv(extracted.features,"tidydata.txt")
 averages <- write.csv(averages,"average_means_and_standard_devs.txt")
